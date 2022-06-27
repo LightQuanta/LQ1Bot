736ee05e56e46fb0f7f0d80235f74a6f9a43bb04
Light_Quanta <ZCH22248@foxmail.com>
2022-06-27 21:17:15 +0800

    升级警钟功能 支持添加多个警钟 支持发送指定警钟 支持编钟功能，同时发送所有警钟

--- LQ1Bot/Plugins/Alarm.cs
+++ LQ1Bot/Plugins/Alarm.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Text;
@@ -27,12 +28,12 @@ namespace LQ1Bot.Plugins {
             if ((text == "!delalarm") && PermissionMgr.IsGroupOrBotAdmin(e.Sender)) {
                 if (AlarmExists(q)) {
                     File.Delete($"alarmcfg/{q}.txt");
-                    await MessageManager.SendGroupMessageAsync(q, "已移除警钟");
+                    await MessageManager.SendGroupMessageAsync(q, "已移除所有警钟");
                 } else {
                     await MessageManager.SendGroupMessageAsync(q, "本群还未添加警钟！");
                 }
             }
-            if (text.StartsWith("!setalarm ") && PermissionMgr.IsGroupOrBotAdmin(e.Sender)) {
+            if (text.StartsWith("!addalarm ") && PermissionMgr.IsGroupOrBotAdmin(e.Sender)) {
                 string alarm = text[10..];
                 if (alarm.Length > 0 && alarm.Split("|").Length == 2) {
                     string time = alarm.Split("|")[0];
@@ -48,8 +49,10 @@ namespace LQ1Bot.Plugins {
                         try {
                             DateTime t = new(year, month, date, hour, minute, second);
                             long tick = t.Ticks;
-                            File.WriteAllText($"alarmcfg/{q}.txt", tick + "|" + content);
-                            await MessageManager.SendGroupMessageAsync(q, "成功设置警钟");
+                            List<string> list = new(GetAlarms(q));
+                            list.Add(tick + "|" + content);
+                            File.WriteAllText($"alarmcfg/{q}.txt", string.Join("\n", list));
+                            await MessageManager.SendGroupMessageAsync(q, "成功添加警钟");
                         } catch (ArgumentOutOfRangeException) {
                             await MessageManager.SendGroupMessageAsync(q, "时间输入错误！输入格式应为：\n年-月-日 时:分:秒|回复内容");
                         }
@@ -63,11 +66,17 @@ namespace LQ1Bot.Plugins {
 
             if (text == "!getalarm") {
                 if (AlarmExists(q)) {
-                    string temp = File.ReadAllText($"alarmcfg/{q}.txt");
-                    string time = temp.Split("|")[0];
-                    string content = temp.Split("|")[1];
-                    var InitTime = new DateTime(long.Parse(time));
-                    await MessageManager.SendGroupMessageAsync(q, $"{InitTime.ToString("yyyy-MM-dd HH:mm:ss")}|{content}");
+                    var alarms = GetAlarms(q);
+                    List<string> msg = new();
+
+                    foreach (var v in alarms) {
+                        string temp = v;
+                        string time = temp.Split("|")[0];
+                        string content = temp.Split("|")[1];
+                        var InitTime = new DateTime(long.Parse(time));
+                        msg.Add($"{InitTime:yyyy-MM-dd HH:mm:ss}|{content}");
+                    }
+                    await MessageManager.SendGroupMessageAsync(q, string.Join("\n", msg));
                 } else {
                     await MessageManager.SendGroupMessageAsync(q, "本群还未添加警钟！");
                 }
@@ -75,7 +84,7 @@ namespace LQ1Bot.Plugins {
 
             if (text == "警钟" || text == "警钟长鸣" || text == "警钟敲烂") {
                 if (AlarmExists(q)) {
-                    string temp = File.ReadAllText($"alarmcfg/{q}.txt");
+                    string temp = GetAlarms(q)[^1];
                     string time = temp.Split("|")[0];
                     string content = temp.Split("|")[1];
                     var InitTime = new DateTime(long.Parse(time));
@@ -87,20 +96,57 @@ namespace LQ1Bot.Plugins {
                     await MessageManager.SendGroupMessageAsync(q, "本群还没有警钟！");
                 }
             }
+            var match = Regex.Match(text, @"^警钟(\d+)$");
+            if (match.Success) {
+                if (AlarmExists(q)) {
+                    var alarms = GetAlarms(q);
+                    int id = int.Parse(match.Groups[1].Value);
+                    if (id >= 1 && id <= alarms.Length) {
+                        string temp = GetAlarms(q)[id - 1];
+                        string time = temp.Split("|")[0];
+                        string content = temp.Split("|")[1];
+                        var InitTime = new DateTime(long.Parse(time));
+                        var Now = DateTime.Now;
+                        var Diff = Now - InitTime;
+                        string DiffStr = $"{Diff.Days}天{Diff.Hours}小时{Diff.Minutes}分{Diff.Seconds}秒{Diff.Ticks % 10000000 / 10000}毫秒{Diff.Ticks % 10000 / 10}微秒{Diff.Ticks % 10}00纳秒";
+                        await MessageManager.SendGroupMessageAsync(q, content.Replace("{time}", DiffStr));
+                    } else {
+                        await MessageManager.SendGroupMessageAsync(q, "不存在该警钟！");
+                    }
+                } else {
+                    await MessageManager.SendGroupMessageAsync(q, "本群还没有警钟！");
+                }
+            }
+
+            if (text == "编钟") {
+                if (AlarmExists(q)) {
+                    var alarms = GetAlarms(q);
+                    List<string> msg = new();
+
+                    foreach (var v in alarms) {
+                        string temp = v;
+                        string time = temp.Split("|")[0];
+                        string content = temp.Split("|")[1];
+                        var InitTime = new DateTime(long.Parse(time));
+                        var Now = DateTime.Now;
+                        var Diff = Now - InitTime;
+                        string DiffStr = $"{Diff.Days}天{Diff.Hours}小时{Diff.Minutes}分{Diff.Seconds}秒{Diff.Ticks % 10000000 / 10000}毫秒{Diff.Ticks % 10000 / 10}微秒{Diff.Ticks % 10}00纳秒";
+                        msg.Add(content.Replace("{time}", DiffStr));
+                    }
+                    await MessageManager.SendGroupMessageAsync(q, string.Join("\n", msg));
+                } else {
+                    await MessageManager.SendGroupMessageAsync(q, "本群还没有警钟！");
+                }
+            }
             return false;
         }
 
-        private bool AlarmExists(string q) {
+        private static string[] GetAlarms(string q) {
+            return File.ReadAllLines($"alarmcfg/{q}.txt");
+        }
+        private static bool AlarmExists(string q) {
             return File.Exists($"alarmcfg/{q}.txt");
         }
 
-        private void Init(string q) {
-            if (!File.Exists($"alarmcfg/{q}.txt")) {
-                if (!Directory.Exists("alarmcfg")) {
-                    Directory.CreateDirectory("alarmcfg");
-                }
-                File.Create($"alarmcfg/{q}.txt");
-            }
-        }
     }
 }
